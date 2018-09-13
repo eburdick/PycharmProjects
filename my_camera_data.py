78d16cdb97e0262705a10b37da3b693755591815
Ed Burdick <ed@edburdick.net>
2018-09-13 17:54:52 -0400

    Initial commit. This module defines the camera information for the site.  This includes the name of each camera, a regex pattern for recognizing the memory card labels, and the path to the repository directory for each camera.  It might be good to also put the file extension definitions here so whoever adds the cameras can also specify the file formats created by those cameras.

---
+++ my_camera_data.py
@@ -0,0 +1,26 @@
+import re
+#
+# starting camera and memory card information for our cameras...
+#
+# This takes the form of a list of dictionaries, one list element for each camera. Each dictionary contains
+#    'name' The name of the camera
+#    'card_pattern' A regular expression pattern that matches the volume label of that camera's memory card(s)
+#    'repository_base' The path to the top of that camera's directory tree on the computer
+#
+# note this is the starting form of these dictionaries.  More will be added if we find memory cards associated
+# with these cameras as follows:
+#    'processed': Initialized to False and set to True when this dictionary is processed in any way
+#    'files_with_times': Initialized as an empty list. Records the media files on the memory cards for this cameraa
+#                        and their creation timestamps
+#    'card_path_list': a list of paths to memory cards for this camera eg ['H:\','J:\']
+#    'new_repository_dir': path to directory for new files named from today's date, eg
+#                    'V:\Camera-buf\nikon-d500\renamed copies of flash memory\2018-07-27'
+camera_info = [
+    {'name': 'Nikon D500',
+     'card_pattern': re.compile('.*d500', re.IGNORECASE),  # pattern: any characters followed by "d500"
+     'repository_base': 'V:\\Camera-buf\\nikon-d500\\renamed copies of flash memory\\',
+     'digital_camera_image_path': 'DCIM\\'},  # camera card directory containing media files
+    {'name': 'Nikon Coolpix B700',
+     'card_pattern': re.compile('.*b700', re.IGNORECASE),  # pattern: any characters followed by "b700"
+     'repository_base': 'V:\\Camera-buf\\nikon-coolpix-b700\\renamed copies of flash memory\\',
+     'digital_camera_image_path': 'DCIM\\'}]
