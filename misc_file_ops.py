682615f420684ab02ab7ac01dc5f7506a6eecde8
eburdick <ed@edburdick.net>
2019-07-13 21:35:10 -0400

    First check-in of this code.  This is for trying out and creating code snippits that use portable file and directory operations.

---
+++ misc_file_ops.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#
+# Prototype code for portable file operations, just to have code to cut and
+# paste for other programs.
+#
+#
+# Import library modules
+#
+import os
+import shutil          # high level file operations like copy, rename, etc
